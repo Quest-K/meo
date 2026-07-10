23d5bb60a595ac13e10b28329506f3ae7a074eb9
Meo <kjo36234311@gmail.com>
2026-07-10 17:41:12 +0900

    supabase-client.js 업데이트

--- supabase-client.js
+++ supabase-client.js
@@ -5,4 +5,10 @@
 const SUPABASE_URL = "https://kpooxiwiqkcizhtdleor.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwb294aXdpcWtjaXpodGRsZW9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MjA2MTEsImV4cCI6MjA5OTE5NjYxMX0.4aBRHVw2OK9mfX78zbfSyxqKRcYnvejCGH22MyrVTA8";
 
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: false
+  }
+});
